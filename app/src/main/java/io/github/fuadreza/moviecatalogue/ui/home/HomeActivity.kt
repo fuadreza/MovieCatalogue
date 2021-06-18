7530466c0145863bee925aceafa80dbc14efee34
fuadreza <fuadreza1011@gmail.com>
2021-06-19 01:11:54 +0700

    add favorite button, add favorite layout

--- app/src/main/java/io/github/fuadreza/moviecatalogue/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/fuadreza/moviecatalogue/ui/home/HomeActivity.kt
@@ -15,8 +15,6 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val sectionPagerAdapter = HomeSectionPagerAdapter(this, supportFragmentManager)
         binding.viewPager.adapter = sectionPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewPager)
